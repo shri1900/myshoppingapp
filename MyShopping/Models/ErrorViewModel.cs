ef56000e656fd8b50c7fcd3ea3bc08bb9b40b31e
Thombre Shrikant <shrikant.thombre@tieto.com>
2021-04-17 22:05:58 +0530

    First Commit

---
+++ MyShopping/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyShopping.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
